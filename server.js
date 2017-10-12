a66326b1d38523fef7ab58aa49dd565b44c97813
aWahlang <31699089+aWahlang@users.noreply.github.com>
2017-10-11 22:13:10 -0400

    Basic skeletons sever

---
+++ server.js
@@ -0,0 +1,34 @@
+//must have node JS setup on system 
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(bodyParser.json());
+
+
+//creating server
+var server = app.listen(8081, function(){
+	var host = server.address().address;
+	var port = server.address().port;
+
+	console.log("Server running at http://%s:%s",host,port);
+})
+
+//basic routes
+app.get('/', function(request, response){
+	response.sendFile(__dirname + '/HTML/index.html');
+	console.log("test.html sent");
+});
+
+//route for username check
+app.post('/username', function(request, response){
+	var username = request.body;
+	console.log(request.body);
+	//check username with data base and respond with salt  
+
+
+})
+
