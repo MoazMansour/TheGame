e868667df6b8475d24da0d1127fde78284301d8e
sayudhroy <32206290+sayudhroy@users.noreply.github.com>
2017-11-29 14:29:10 -0500

    Added buildings and colored & annotated map

--- resources/templates/game/game.js
+++ resources/templates/game/game.js
@@ -70,7 +70,7 @@ function player(width, height, color, x, y) {
     this.height = height;
     this.x = x;
     this.y = y;
-    this.speed = 5;/* change back to 1 after testing */
+    this.speed = 1; /* change back to 1 after testing */
     this.move = function() {
         if(keys[83] && !this.collisionCheck(this.x, this.y + 1, this.width, this.height)) this.y += this.speed;
         if(keys[87] && !this.collisionCheck(this.x, this.y - 1, this.width, this.height)) this.y -= this.speed;
@@ -79,9 +79,9 @@ function player(width, height, color, x, y) {
     }
     this.collisionCheck = function(x, y, t, h) {
         // TODO: ADJUST UPPER BOUNDS FOR SIZE OF MAP
-        if(x < 0 || x > 1490)
+        if(x < 0 || x > 2180)
             return true;
-        if(y < 0 || y > 1690)
+        if(y < 0 || y > 2180)
             return true;
         for (var i = 0, len = buildings.length; i < len; i++) {
             if(collision(x, y, t, h, buildings[i])) {
@@ -114,13 +114,18 @@ function building(width, height, x, y) {
     this.x = x;
     this.y = y;
     // ----- USED TO PHYSICALLY DRAW BUILDINGS (FOR TESTING) --------
+    /*
     this.update = function(x, y) {
         ctx = map.context;
         ctx.fillStyle = "black";
+        ctx.opacity = 0.5;
         console.log("x " + x);
         console.log("y " + y);
+        ctx.globalAlpha = 0.2;
         ctx.fillRect(this.x - (x - 250), this.y - (y - 200), this.width, this.height);
+        ctx.globalAlpha = 1.0;
     }
+    */
 }
 
 function opponent(username, color, x, y, width, height) {
@@ -142,9 +147,11 @@ function updateGameLocal() {
     map.update(myPlayer.x, myPlayer.y, myPlayer.color);
 
     // ----- USED TO PHYSICALLY DRAW BUILDINGS (FOR TESTING) --------
+    /*
     for (var i = 0, len = buildings.length; i < len; i++) {
        buildings[i].update(myPlayer.x, myPlayer.y);
     }
+    */
 
     for (var i = 0, len = opponents.length; i < len; i++) {
         if (opponents[i].userName != myUserName)
@@ -200,8 +207,52 @@ function parseCookieData(key) {
 }
 
 function loadBuildings() {
-    buildings[0] = new building(150, 70, 200, 50);
-    //buildings[1] = new building(width, height, x, y);
+    buildings[0] = new building(60, 95, 700, 180);      //Anderson
+    buildings[1] = new building(75, 80, 785, 55);       //Wilder
+    buildings[2] = new building(105, 105, 810, 180);    //Sage Art
+    buildings[3] = new building(100, 157, 787, 452);    //Spurrier
+    buildings[4] = new building(267, 275, 922, 452);    //D.G.G.H.M.
+    buildings[5] = new building(250, 72, 483, 870);     //Fauver Stadium
+    buildings[6] = new building(477, 243, 407, 992);    //R.B.G Athletic
+    buildings[7] = new building(190, 165, 300, 1265);   //Gilbert
+    buildings[8] = new building(137, 130, 525, 1300);   //Hoeing
+    buildings[9] = new building(65, 120, 705, 1310);    //Crosby
+    buildings[10] = new building(115, 153, 368, 1457);  //Tiernan
+    buildings[11] = new building(135, 128, 527, 1481);  //Lovejoy
+    buildings[12] = new building(65, 120, 705, 1484);   //Burton
+    buildings[13] = new building(77, 43, 431, 1645);    //Frat-1
+    buildings[14] = new building(52, 55, 798, 1659);    //Frat-2
+    buildings[15] = new building(55, 37, 704, 1672);    //Frat-3
+    buildings[16] = new building(60, 60, 625, 1650);    //Frat-4
+    buildings[17] = new building(65, 47, 536, 1658);    //Frat-5
+    buildings[18] = new building(58, 62, 544, 1727);    //Frat-6
+    buildings[19] = new building(42, 45, 625, 1808);    //Frat-7
+    buildings[20] = new building(53, 45, 705, 1817);    //Frat-8
+    buildings[21] = new building(60, 45, 790, 1817);    //Frat-9
+    buildings[22] = new building(88, 120, 927, 1704);   //Todd
+    buildings[23] = new building(125, 68, 1041, 1732);  //Strong Aud.
+    buildings[24] = new building(215, 105, 1264, 1726); //Gleason/Schlegel
+    buildings[25] = new building(168, 85, 1409, 1846);  //Wallis
+    buildings[26] = new building(80, 123, 1545, 1603);  //Hopeman
+    buildings[27] = new building(165, 45, 1549, 1377);  //Gavett-1
+    buildings[28] = new building(110, 142, 1549, 1421); //Gavett-2
+    buildings[29] = new building(70, 122, 1696, 1450);  //Taylor
+    buildings[30] = new building(102, 182, 1808, 1570); //Wilmot/Georgen
+    buildings[31] = new building(41, 80, 1808, 1477);   //Random-Wil/Geor
+    buildings[32] = new building(287, 254, 1718, 1774); //Computer Quad
+    buildings[33] = new building(67, 135, 1550, 1209);  //Harkness
+    buildings[34] = new building(93, 188, 1403, 1049);  //Meliora
+    buildings[35] = new building(205, 385, 1307, 1267); //B&L/H/CGS/D
+    buildings[36] = new building(41, 172, 1100, 1488);  //Lattimore
+    buildings[37] = new building(41, 165, 1100, 1266);  //Morey
+    buildings[38] = new building(82, 43, 1018, 1549);   //Rettner
+    buildings[39] = new building(15, 60, 1126, 1429);   //Morey-Lattimore
+    buildings[40] = new building(115, 145, 940, 1224);  //WilCo
+    buildings[41] = new building(144, 125, 983, 1044);  //Douglass
+    buildings[42] = new building(14, 57, 1026, 1168);   //WilCo-Douglass
+    buildings[43] = new building(155, 250, 1152, 987);  //RushRhees
+
+    //buildings[n] = new building(width, height, x, y);
 }
 
 startGame();
