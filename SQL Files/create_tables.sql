dd2b98b136c73e4e78523652ed3e4a5c7b961d52
Moaz Mansour <31941060+MoazMansour@users.noreply.github.com>
2017-12-01 17:45:34 -0500

    Coins_location

--- SQL Files/create_tables.sql
+++ SQL Files/create_tables.sql
@@ -21,7 +21,7 @@ drop table if exists users;
 CREATE TABLE IF NOT EXISTS users(
     user_id INT(6) ZEROFILL NOT NULL,
     username VARCHAR(20) UNIQUE NOT NULL,
-    color VARCHAR(50) UNIQUE NOT NULL,
+    color VARCHAR(50) NOT NULL,
     email VARCHAR(100) DEFAULT NULL,
     hash  VARCHAR(100)    NOT NULL,
     salt  VARCHAR(100)    NOT NULL,
